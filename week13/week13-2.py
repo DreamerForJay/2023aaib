d9cca2ee8273404e8c61b7e44a81bb82dec9061d
DreamerForJay <cl.yang04@gmail.com>
2023-12-08 11:45:45 +0800

    week07

---
+++ week13/week13-2.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        #要用之前教過的函式呼叫函式
+        if root ==None:return ""
+        now = str(root.val)
+        left = self.tree2str(root.left)
+        right = self.tree2str(root.right)
+        #"("")"
+
+        if left=="" and right=="":return now
+        if right=="":return now+"("+left+")"
+        return now+ "("+left+")("+right+")"
+
+        
+        return now+"("+left+")("+right+")"
---
+++ week13/week13-4c.py
@@ -0,0 +1,4 @@
+#(SOIT106_ADVANCE_011)3 
+a = int(input(),2)
+
+print(a)
